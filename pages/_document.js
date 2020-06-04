2dd8b93cbece740b1e2e93f0784c414fb8de0423
checktheroads <matt@allroads.io>
2020-06-04 11:12:29 -0700

    fix default body style

--- pages/_document.js
+++ pages/_document.js
@@ -11,12 +11,19 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://www.google-analytics.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+          <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;700&family=Inter:wght@200;400;600;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400&family=Inter:wght@200;400;600;800&display=swap"
             rel="stylesheet"
           />
+          <link rel="preconnect" href="https://www.google-analytics.com" />
+          <style>{`
+          body {
+              margin: 0;
+            }
+          `}</style>
         </Head>
         <body>
           <Main />
