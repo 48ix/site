7c6373abf26e9d1e025ff0255ff809d55fb86435
checktheroads <matt@allroads.io>
2020-06-04 11:12:48 -0700

    fix invalid sitemap

--- plugins/generateSitemap.js
+++ plugins/generateSitemap.js
@@ -4,9 +4,9 @@ const globby = require('globby');
 const prettier = require('prettier');
 
 (async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+  const prettierConfig = await prettier.resolveConfig('./prettier.config.js');
 
-  const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js']);
+  const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js', '!pages/subscribe/*']);
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -14,12 +14,12 @@ const prettier = require('prettier');
               .map(page => {
                 const path = page.replace('pages', '').replace('.js', '').replace('.mdx', '');
                 const route = path === '/index' ? '' : path;
-
+                console.log(path, route);
                 return `
-                        <url>
-                            <loc>${`https://48ix.net${route}`}</loc>
-                        </url>
-                    `;
+                  <url>
+                      <loc>${`https://48ix.net${route}`}</loc>
+                  </url>
+              `;
               })
               .join('')}
         </urlset>
